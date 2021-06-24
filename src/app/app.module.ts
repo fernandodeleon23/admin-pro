536245fe6652c3bcaac799521a1dfe3b39782a97
= <=>
2021-06-24 00:17:58 -0400

    Fin de la secction 3 de diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './parts/header/header.component';
+import { SidebarComponent } from './parts/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './parts/breadcrumbs/breadcrumbs.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
